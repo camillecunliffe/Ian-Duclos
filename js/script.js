813e6ee742afd8548c423161f2109c4fbedde0b4
camillecunliffe <97906430+camillecunliffe@users.noreply.github.com>
2022-01-17 17:36:56 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,17 +8,30 @@
         'img/Ferreira_text.jpg',
         'img/Fontes_pic.jpg',
         'img/Fontes_text.jpg',
-        'img/Freitas_pic.jpg',
+        'img/Freitas_pic.jpg', 
+        'img/Gröpper_pic.jpg',
+        'img/Gröpper_text.jpg',
+        'img/guimaraes_pic.jpg',
+        'img/guimaraes_text.jpg',
+        'img/Jesus_pic.jpg',
+        'img/Jesus_text.jpg',
+        'img/Martins_pic.jpg',
     ];
 
     var Images2 = [
-        'img/Duclos_pic.jpg',
-        'img/Duclos_text.jpg',
-        'img/Ferreira_pic.jpg',
-        'img/Ferreira_text.jpg',
-        'img/Fontes_pic.jpg',
-        'img/Fontes_text.jpg',
-        'img/Freitas_pic.jpg',
+        'img/Martins_text.jpg',
+        'img/Mesquita_pic.jpg',
+        'img/Mesquita_text.jpg',
+        'img/Monteiro_Judgement_pic.jpg',
+        'img/Monteiro_Judgement_text.jpg',
+        'img/Monteiro_TheJudge_pic.jpg',
+        'img/Monteiro_TheJudge_text.jpg',
+        'img/Monteiro_TheSun_pic.jpg',
+        'img/Monteiro_TheSun_text.jpg', 
+        'img/Souza_pic.jpg',
+        'img/Souza_text.jpg',
+        'img/Torresan_pic.jpg',
+        'img/Torresan_text.jpg',
     ];
 
     function getRandomImage(images) {
