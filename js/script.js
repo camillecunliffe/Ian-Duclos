751a17c22ceba7c6c6d37116be3a8d1aa68e4ce7
camillecunliffe <97906430+camillecunliffe@users.noreply.github.com>
2022-01-17 16:22:14 -0500

    Summary

--- js/script.js
+++ js/script.js
@@ -11,6 +11,16 @@
         'img/Freitas_pic.jpg',
     ];
 
+    var Images2 = [
+        'img/Duclos_pic.jpg',
+        'img/Duclos_text.jpg',
+        'img/Ferreira_pic.jpg',
+        'img/Ferreira_text.jpg',
+        'img/Fontes_pic.jpg',
+        'img/Fontes_text.jpg',
+        'img/Freitas_pic.jpg',
+    ];
+
     function getRandomImage(images) {
         var num = Math.floor(Math.random() * images.length),
             pic = images[num],
@@ -20,5 +30,6 @@
     }
 
     $("#picture").append(getRandomImage(Images));
+    $("#picture2").append(getRandomImage(Images2));
 
 })();
